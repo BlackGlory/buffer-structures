67221ef912c16bf8d6e6e260623bdff5318f5167
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-01-07 09:57:06 +0000

    perf: init benchmark

---
+++ benches/hash-map.ts
@@ -0,0 +1,48 @@
+import { go } from '@blackglory/go'
+import { Allocator, HashMap, Uint16, Uint16View } from '..'
+import { Benchmark } from 'extra-benchmark'
+
+const benchmark = new Benchmark('HashMap')
+
+go(async () => {
+  benchmark.addCase('Map#set', () => {
+    const count = 10000
+    const map = new Map<number, number>()
+
+    return {
+      beforeEach() {
+        map.clear()
+      }
+    , iterate() {
+        for (let i = 0; i < count; i++) {
+          map.set(i, i)
+        }
+      }
+    }
+  })
+
+  benchmark.addCase('HashMap#set', () => {
+    const count = 10000
+    let allocator: Allocator<ArrayBuffer>
+    let map: HashMap<Uint16View, Uint16View>
+
+    debugger
+    return {
+      beforeEach() {
+        allocator = new Allocator(new ArrayBuffer(Uint16View.byteLength * count * count))
+        map = new HashMap(allocator, Uint16View, count)
+      }
+    , iterate() {
+        for (let i = 0; i < count; i++) {
+          const view = new Uint16(allocator, i)
+          map.set(view._view, view._view)
+        }
+      }
+    }
+  })
+
+  console.log(`Benchmark: ${benchmark.name}`)
+  for await (const result of benchmark.run()) {
+    console.log(result)
+  }
+})
