8af0279254ae6adee2ae9d018aabeef649b63341
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-11-22 09:19:23 +0000

    docs: update

--- src/hasher.ts
+++ src/hasher.ts
@@ -2,8 +2,9 @@ import { IHasher } from '@src/interfaces.js'
 import { NULL } from '@src/null.js'
 import { h32 } from 'xxhashjs'
 
-// 一个令人惊讶的事实是, 在JIT的帮助下,
-// `xxhashjs`这个纯JavaScript实现比本机插件`@node-rs/xxhash`要快.
+// 在本项目的基准测试里,
+// 基于纯JavaScript实现xxhashjs的Hasher比基于本机插件@node-rs/xxhash的Hasher要快得多.
+// 这与直觉以及单独测试这两个库的结果不符, 猜测可能是JIT优化巧合地影响了基准测试.
 export class Hasher implements IHasher {
   private hasher = h32(0)
 
